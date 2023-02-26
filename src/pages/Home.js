f18a66bbffeb21b5795f2d68ccc78ee74b5b95f5
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-02-26 21:34:07 +0200

    create HomePage

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Head from './../components/HeadTitle/Head';
+
+const Home = () => {
+    return (
+        <Head title={'(home)'}>home</Head>
+    )
+}
+
+export default Home
