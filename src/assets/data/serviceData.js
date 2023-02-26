ee6cdbf291c5423eb7c1d71bda69ce2e40685725
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-02-26 23:49:49 +0200

    update in icons

--- src/assets/data/serviceData.js
+++ src/assets/data/serviceData.js
@@ -1,24 +1,26 @@
+import { faTruckFieldUn, faRotate, faShieldHalved, faDollarSign } from '@fortawesome/free-solid-svg-icons'
+
 const serviceData = [
   {
-    icon: "ri-truck-line",
+    icon: faTruckFieldUn,
     title: "Free Shipping",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#fdefe6",
   },
   {
-    icon: "ri-refresh-line",
+    icon: faRotate,
     title: "Easy Returns",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#ceebe9",
   },
   {
-    icon: "ri-secure-payment-line",
+    icon: faShieldHalved,
     title: "Secure Payment",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#e2f2b2",
   },
   {
-    icon: "ri-exchange-dollar-line",
+    icon: faDollarSign,
     title: " Back Guarantee",
     subtitle: "Lorem ipsum dolor sit amet.",
     bg: "#d6e5fb",
