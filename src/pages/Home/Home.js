77ed86af24a45c8745f9e1a5a188ee4a06451c73
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-02-26 23:50:49 +0200

    add Services component in app file

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Home.css';
 import Head from '../../components/HeadTitle/Head';
 import HeroSection from '../../components/Home/HeroSection';
-import './Home.css';
+import Services from './../../components/Services/Services';
 
 const Home = () => {
     return (
         <Head title={'(home)'}>
             <HeroSection/>
+            <Services/>
         </Head>
     )
 }
