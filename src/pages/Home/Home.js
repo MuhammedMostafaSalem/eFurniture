258850d123b7351d7782f437f3841fe2a2859d7c
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-02-27 02:15:31 +0200

    add TrendingProducts component in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,14 @@ import './Home.css';
 import Head from '../../components/HeadTitle/Head';
 import HeroSection from '../../components/Home/HeroSection';
 import Services from './../../components/Services/Services';
+import TrendingProducts from './../../components/Home/TrendingProducts';
 
 const Home = () => {
     return (
         <Head title={'(home)'}>
             <HeroSection/>
             <Services/>
+            <TrendingProducts/>
         </Head>
     )
 }
