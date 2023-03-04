78f32f081a72934eb5e0c5ef02795987c416cf88
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-04 08:23:52 +0200

    add shop page in app file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Utility/Header/Header";
 import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
 import Footer from './components/Utility/Footer/Footer';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Routes>
         <Route index element={<Home/>}/>
+        <Route path='shop' element={<Shop/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
