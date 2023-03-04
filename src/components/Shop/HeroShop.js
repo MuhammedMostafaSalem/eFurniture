48e9202527407aa937be9f1fa2187496151fc37f
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-04 08:24:47 +0200

    create hero shop

---
+++ src/components/Shop/HeroShop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import heroShop from '../../assets/images/hero-shop.jpg'
+
+const HeroShop = () => {
+    return (
+        <div className='heroShop_section'>
+            <img src={heroShop} alt='' />
+            <h1 className='subTitle'>products</h1>
+        </div>
+    )
+}
+
+export default HeroShop
