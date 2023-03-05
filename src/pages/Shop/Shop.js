1684448ce6b27e7436ae9dc7bc75b87d137f8da8
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-05 10:04:52 +0200

    add FilterSearch component in shop page file

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Shop.css';
 import Head from './../../components/HeadTitle/Head';
 import HeroShop from '../../components/Shop/HeroShop';
+import FilterSearch from '../../components/Shop/FilterSearch';
 
 const Shop = () => {
     return (
         <Head title={'(shop)'}>
             <HeroShop/>
+            <FilterSearch/>
         </Head>
     )
 }
